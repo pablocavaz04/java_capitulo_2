b1ca1012c8b7834ca5766b0ad3a6cd2e2209dfe2
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-11 13:23:45 +0200

    Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,15 @@
+/**
+ * Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. Muestra los valores de esas variables por pantalla de tal forma que el
+resultado del programa sea el mismo que en el ejercicio 2.
+Author: Pablo Camino Vázquez
+ */
+
+
+public class Ejercicio3 {
+    public static void main(String [] args ){
+        String nombre = "Pablo Camino Vázquez";
+        String direccion = "Pasillo de sierra blanca";
+        String telefono = "618381824";
+        System.out.println("Me llamo "+nombre+" vivo en la calle "+direccion+" y mi num de telefono es : "+telefono);
+    }
+}
