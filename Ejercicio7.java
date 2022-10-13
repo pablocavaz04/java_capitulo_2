a7c1360c3b269857e534656fdc6bfb9f52e9e6c2
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-13 09:12:26 +0200

    FIn

---
+++ Ejercicio7.java
@@ -0,0 +1,8 @@
+public class Ejercicio7 {
+    public static void main(String [] args){
+        char letra1 = 'p';
+        char letra5 = 'o';
+        String nombre = "pablo";
+        System.out.println(letra1+" "+letra5+" "+nombre);
+    }
+}
