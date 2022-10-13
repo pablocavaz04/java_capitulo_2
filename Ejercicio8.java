f7b5d5096bdff256b0819da278b5d7770355e51e
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-13 09:16:11 +0200

    Finalizado

---
+++ Ejercicio8.java
@@ -0,0 +1,12 @@
+public class Ejercicio8 {
+    public static void main(String [] args){
+        char letra1 = 'p';
+        char letra2 = 'a';
+        char letra3 = 'b';
+        char letra4 = 'l';
+        char letra5 = 'o';
+
+        String nombre = " "+letra1+letra2+letra3+letra4+letra5;
+        System.out.println(nombre);
+    }
+}
