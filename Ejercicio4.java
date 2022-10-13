aa5f5d4760f63cb56944f3fde8914f5ad6049f34
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-13 08:51:10 +0200

    Finalizado

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /*
 *Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere
 convertir deberá estar almacenada en una variable.*/
@@ -9,6 +7,7 @@ public class Ejercicio4 {
         Scanner sc = new Scanner(System.in);
         System.out.println(" Introduzca una cantidad de euros: ");
         double euros = sc.nextDouble();
-        System.out.println("La cantidad introducida de "+euros+" €, equivale a la cantidad de "+(euros*166)+" pesetas");
+        System.out.println("La cantidad introducida de "+euros+" €, equivale a la cantidad de "+(int)(euros*166)+" pesetas");
+        sc.close();
     }
 }
--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
 public class Ejercicio5 {
-    
+    public static void main (String [] args ){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Introduzca una cantidad de pesetas: ");
+        double pesetas = sc.nextDouble();
+        System.out.println("La cantidad introducida de "+pesetas+" pesetas, equivale a la cantidad de "+(int)(pesetas/166)+" €");
+        sc.close();
+    }
 }
