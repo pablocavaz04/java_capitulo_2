8d3ac8aaa0442340d8d52df74025d1bf4d612115
pablocavaz04 <camvazpablo04@gmail.com>
2022-10-13 09:07:34 +0200

    Ejercicio6

---
+++ Ejercicio6.java
@@ -0,0 +1,10 @@
+public class Ejercicio6 {
+    public static void main(String[] args){
+        double Base_i = 65.75;
+        System.out.printf("Base imponible %8.2f\n", Base_i);
+        System.out.printf("IVA %8.2f\n", (Base_i*0.21));
+        System.out.println("  ");
+        System.out.printf("Total %8.2f\n",(Base_i* 1.21));
+        
+    }
+}
